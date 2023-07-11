8c05fdc88190cefcda2012490474d850e1000cbc
sik9252 <sik9252@naver.com>
2023-07-11 13:45:15 +0900

    feat: 모든 지하철 호선 버튼 추가

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,9 +1,34 @@
-import { Box } from "@chakra-ui/react";
+import { useState, useContext } from "react";
+import { Flex, Box, Button } from "@chakra-ui/react";
+import { lineList } from "../../lib/data/line.js";
+import { SelectedLineContext } from "../../context/selectedLineContext.js";
 
 function SideMenu() {
+  const selectedLineContext = useContext(SelectedLineContext);
+
+  const [line, setLine] = useState(lineList);
+
+  const clickLine = (lineName: string) => {
+    selectedLineContext?.selectLine(lineName);
+  };
+
   return (
     <Box w="15%" h="100vh">
-      SideMenu
+      {line.map((line) => (
+        <Flex key={line.id} align={"center"} my="5px">
+          <img src={line.icon} width="25" height="25" />
+          <Button
+            height="48px"
+            width="100%"
+            border="1px"
+            colorScheme="gray"
+            mx="5px"
+            onClick={() => clickLine(line.name)}
+          >
+            {line.name}
+          </Button>
+        </Flex>
+      ))}
     </Box>
   );
 }
