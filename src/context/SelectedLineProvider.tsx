051970d7e933f5c298ab8bfc7f87908a73c54d1f
sik9252 <sik9252@naver.com>
2023-07-11 13:43:31 +0900

    feat: 선택한 호선에 따른 노선도를 보여주기 위한 context API 추가
    - 선택한 호선의 이름을 전역상태로 저장하여 다른 컴포넌트에서 사용할 수 있도록 구현함

---
+++ src/context/SelectedLineProvider.tsx
@@ -0,0 +1,17 @@
+import { useState, PropsWithChildren } from "react";
+import { SelectedLineContext } from "./selectedLineContext";
+
+export function SelectLineProvider({ children }: PropsWithChildren) {
+  const [selectedLine, setSelectedLine] = useState<string>("1호선");
+
+  const selectLine = (line: string) => {
+    setSelectedLine(line);
+    // 나중에 여기에 선택된 호선에 따라 알맞는 노선도 이미지 내려주는 코드 작성하기
+  };
+
+  return (
+    <SelectedLineContext.Provider value={{ selectedLine, selectLine }}>
+      {children}
+    </SelectedLineContext.Provider>
+  );
+}
